796bce1fc99c2eded3889aff02bf9ef2bea279a6
Luis Martins <luis.faria.martins@gmail.com>
2024-04-07 21:18:47 +0100

    Nvim config
    Nvim config using packer
    Plugins and Keybings configurations.
    Plugins: Fugitive, harpoon, lsp, lualine, telescope, treesitter, trouble, twilight and undotree

---
+++ nvim/after/plugin/lualine.lua
@@ -0,0 +1,114 @@
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+    blue   = '#80a0ff',
+    black  = '#080808',
+    white  = '#c6c6c6',
+    grey   = '#303030',
+}
+
+local bubbles_theme = {
+    normal = {
+        a = { fg = colors.black, bg = colors.blue },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.black, bg = colors.black }, -- or white, grey
+    },
+
+    insert = { a = { fg = colors.black, bg = colors.blue } },
+    visual = { a = { fg = colors.black, bg = colors.blue } },
+    replace = { a = { fg = colors.black, bg = colors.blue } },
+
+    inactive = {
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
+        c = { fg = colors.black, bg = colors.black },
+    },
+}
+
+require('lualine').setup {
+    options = {
+        theme = bubbles_theme,
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+    },
+    refresh = { 
+        statusline = 1000,
+        tabline = 1000,  
+        winbar = 1000
+    },
+    sections = {
+        lualine_a = {
+            { 
+                'filename', 
+                separator = { left = '' }, 
+                right_padding = 2,
+                file_status = true,      -- Displays file status (readonly status, modified status)
+                newfile_status = false,  -- Display new file status (new file means no write after created)
+                path = 0,                -- 0: Just the filename
+                -- 1: Relative path
+                -- 2: Absolute path
+                -- 3: Absolute path, with tilde as the home directory
+                -- 4: Filename and parent dir, with tilde as the home directory
+
+                shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                -- for other components. (terrible name, any suggestions?)
+                symbols = {
+                    modified = '[+]',      -- Text to show when the file is modified.
+                    readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                    newfile = '[New]',     -- Text to show for newly created file before first write
+                }
+            }
+        },
+        lualine_b = { 
+            'branch',
+            {
+
+                'diff',
+                colored = true,
+                diff_color = {
+                    added    = 'LuaLineDiffAdd',    -- Changes the diff's added color
+                    modified = 'LuaLineDiffChange', -- Changes the diff's modified color
+                    removed  = 'LuaLineDiffDelete', -- Changes the diff's removed color you
+                },
+                symbols = {added = '+', modified = '~', removed = '-'},
+            }
+        },
+        lualine_c = { 'fileformat' },
+        lualine_x = {
+
+            { 
+                'diagnostics', 
+                sources = { 'nvim_diagnostic', 'coc' },
+                sections = { 'error', 'warn', 'info', 'hint' },
+                diagnostics_color = {
+                    error = 'DiagnosticError', -- Changes diagnostics' error color.
+                    warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+                    info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+                    hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+                },
+                symbols = {error = ' ', warn = ' ', info = 'I', hint = 'H'},
+                colored = true,           -- Displays diagnostics status in color if set to true.
+                update_in_insert = false, -- Update diagnostics in insert mode.
+                always_visible = false,   -- Show diagnostics even if there are none.
+            }
+        },
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        },
+    },
+    inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+}
---
+++ nvim/after/plugin/trouble.lua
@@ -0,0 +1,58 @@
+local trouble = require("trouble")
+vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+vim.keymap.set("n", "<leader>xw", function() touble.toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() touble.toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() touble.toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() touble.toggle("loclist") end)
+
+trouble.setup {
+    position = "bottom", -- position of the list can be: bottom, top, left, right
+    height = 10, -- height of the trouble list when position is top or bottom
+    mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+    severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+    group = true, -- group results by file
+    padding = true, -- add an extra new line on top of the list
+    cycle_results = true, -- cycle item list when reaching beginning or end of list
+    action_keys = { -- key mappings for actions in the trouble list
+        close = "q", -- close the list
+        cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r", -- manually refresh
+        jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" }, -- open buffer in new split
+        open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+        open_tab = { "<c-t>" }, -- open buffer in new tab
+        jump_close = {"o"}, -- jump to the diagnostic and close the list
+        toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+        toggle_preview = "P", -- toggle auto_preview
+        hover = "K", -- opens a small popup with the full multiline message
+        preview = "p", -- preview the diagnostic location
+        open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
+        close_folds = {"zM", "zm"}, -- close all folds
+        open_folds = {"zR", "zr"}, -- open all folds
+        toggle_fold = {"zA", "za"}, -- toggle fold of current file
+        previous = "k", -- previous item
+        next = "j" -- next item
+    },
+    multiline = true, -- render multi-line messages
+    win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
+    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    auto_fold = false, -- automatically fold a file trouble list at creation
+    auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+    include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions"  }, -- for the given modes, include the declaration of the current symbol in the results
+    -- settings without a patched font or icons
+    icons = false,
+    fold_open = "v", -- icon used for open folds
+    fold_closed = ">", -- icon used for closed folds
+    indent_lines = false, -- add an indent guide below the fold icons
+    signs = {
+        -- icons / text used for a diagnostic
+        error = "error",
+        warning = "warn",
+        hint = "hint",
+        information = "info"
+    },
+    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+}
