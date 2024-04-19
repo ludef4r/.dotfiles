2b4cc14ed3054b148d9d910354d66a0153c75a53
Luis Martins <luis.faria.martins@gmail.com>
2024-04-19 20:20:27 +0100

    Nerd Font to have webdevicons

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-sudo apt-get install tmux i3 i3lock zsh zsh-syntax-highlighting feh picom nvim polybar
+sudo apt-get install tmux i3 i3lock zsh zsh-syntax-highlighting feh picom nvim polybar ripgrep
+
+sudo apt install wget fontconfig \
+&& wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip \
+&& cd ~/.local/share/fonts && unzip Hack.zip && rm *Windows* && rm Hack.zip && fc-cache -fv
