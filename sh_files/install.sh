02fa3c148e2ecaa80a35291c49d7e7d0155dfdff
202100239 <202100239@estudantes.ips.pt>
2024-07-06 17:12:01 +0100

    Debian version to install Hack nerd font

--- sh_files/install.sh
+++ sh_files/install.sh
@@ -1,5 +1,9 @@
 sudo apt-get install tmux i3 i3lock zsh zsh-syntax-highlighting feh picom nvim polybar ripgrep
 
-sudo apt install wget fontconfig \
-&& wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip \
-&& cd ~/.local/share/fonts && unzip Hack.zip && rm *Windows* && rm Hack.zip && fc-cache -fv
+sudo apt update && sudo apt install wget fontconfig \
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip \
+&& cd ~/.local/share/fonts \
+&& unzip Hack.zip \
+&& rm *Windows* \
+&& rm Hack.zip \
+&& fc-cache -fv
