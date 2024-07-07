720fbb90663b4683114036190ab9066050c3065c
202100239 <202100239@estudantes.ips.pt>
2024-07-07 17:06:02 +0100

    Revised code on sort.sh
    Added comments
    Redone some code so that it makes more sense and works as intended

--- sh_files/sort.sh
+++ sh_files/sort.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-read -p "Select root directory: " directory
+################################################### Get the root directory ###################################################
 
+# Prompt the user to select the root directory with a timeout of 3 minutes (180 seconds)
+read -t 180 -p "Select root directory: " directory
+
+# If no input provided or timeout occurred, set the directory to the user's home directory
 if [ -z "$directory" ]; then
     directory="$HOME"
 fi
 
+# Change to the root directory
+cd "${directory}"
+
+################################################### Check Store directories ##################################################
+
+# Check if the Desktop directory exists
 if [ ! -d "${directory}/Desktop" ]; then
-    cd "${directory}"
     mkdir Desktop
 fi
 
-cd "${directory}/Desktop"
+##################################################### Download directory #####################################################
+
+# Check if the Downloads directory exists
+# If it isn't immediately in the root directory, search for it
+if [ -d "${directory}/Downloads" ]; then
+    cd "${directory}/Downloads"
+else
+    found=$(find "${directory}" -type d -name "Downloads" -print -quit)
+    if [ -n "$found" ]; then
+        cd "$found"
+    else
+        echo "Downloads directory not found."
+        exit 1
+    fi
+fi
+
+
+
+# Code to be revised
 
 if [ ! -d "Certificates" ]; then
     mkdir Certificates
