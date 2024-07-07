406a709988dbbb09c565a48edd71fb2485fb1e59
202100239 <202100239@estudantes.ips.pt>
2024-07-07 14:54:41 +0100

    Code Legibility
    Left two comments to explain
    Note: Changed instalation of tmux to kitty

--- sh_files/install.sh
+++ sh_files/install.sh
@@ -1,5 +1,7 @@
-sudo apt-get install tmux i3 i3lock zsh zsh-syntax-highlighting feh picom nvim polybar ripgrep
+# Install essential packages for the desktop environment and utilities
+sudo apt-get install kitty i3 i3lock zsh zsh-syntax-highlighting feh picom nvim polybar ripgrep
 
+# Install and set Hack Nerd Font
 sudo apt update && sudo apt install wget fontconfig \
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip \
 && cd ~/.local/share/fonts \
