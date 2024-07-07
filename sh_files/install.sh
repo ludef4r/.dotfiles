7f6a5d40844bd207e2b90e98c2c137ca3ebef2df
202100239 <202100239@estudantes.ips.pt>
2024-07-07 15:02:44 +0100

    Code legibility, set of kitty as terminal emulator and zsh as language
    Separation of commands to different lines so that is more readable
    Added code to set kitty as default terminal emulator
    Added code to set zsh as default shell language

--- sh_files/install.sh
+++ sh_files/install.sh
@@ -1,11 +1,22 @@
 # Install essential packages for the desktop environment and utilities
 sudo apt-get install kitty i3 i3lock zsh zsh-syntax-highlighting feh picom nvim polybar ripgrep
 
-# Install and set Hack Nerd Font
-sudo apt update && sudo apt install wget fontconfig \
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip \
-&& cd ~/.local/share/fonts \
-&& unzip Hack.zip \
-&& rm *Windows* \
-&& rm Hack.zip \
-&& fc-cache -fv
+# Update package list and install wget and fontconfig
+sudo apt update
+sudo apt install wget fontconfig
+
+# Download and install Hack Nerd Font
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
+cd ~/.local/share/fonts
+unzip Hack.zip
+rm *Windows*
+rm Hack.zip
+fc-cache -fv
+
+# Set Kitty as the default terminal emulator
+sudo apt-get install dconf-cli
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 50
+sudo update-alternatives --config x-terminal-emulator
+
+# Set Zsh as the default shell for the current user
+chsh -s $(which zsh)
